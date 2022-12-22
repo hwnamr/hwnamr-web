405b79fe77acdce79808c5bc1500c9bdd3051edd
Hawwin Amri <daredeviltf@gmail.com>
2022-12-22 10:39:33 +0700

    added icons

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import {
-  Code,
+  Icon,
   Text,
   Spacer,
   Heading,
@@ -11,20 +11,21 @@ import {
   HStack,
   Link,
 } from "@chakra-ui/react";
+import { FaTwitter, FaLinkedin, FaGithub } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <Container maxWidth="constainer.sm" padding={0} bg="green.500">
+    <Container bg="grey.500">
       <Head>
         <title>HWNAMR</title>
         <link rel="icon" href="/hwnamr.ico" />
       </Head>
       <Flex
+        height="100vh"
         p={10}
         alignItems="center"
         justifyContent="center"
         direction="column"
-        spacing={10}
       >
         <Heading as="h1" mb={6}>
           Hey There!
@@ -36,16 +37,18 @@ export default function Home() {
             alt="Hawwin Amri"
           ></Image>
         </Box>
-        <Heading as="h2" size="lg" mb={6}>
+        <Heading as="h2" size="lg">
           I&apos;m Muhammad Hawwin Amri
         </Heading>
-        <Code mb={6}>Currently learning Web Development and Cybersecurity</Code>
+        <Text mb={6} align="center">
+          Researcher, CTF Player, Infosec Practioner, Perpetual Learner.
+        </Text>
         <Text>--- FOLLOW ME ---</Text>
-        <Flex>
+        <Flex mb={6}>
           <HStack>
             <Box>
               <Link href="https://twitter.com/hwnamr_" isExternal>
-                Twiter
+                <Icon as={FaTwitter} boxSize={3} /> Twitter
               </Link>
             </Box>
             <Spacer />
@@ -54,17 +57,20 @@ export default function Home() {
                 href="https://www.linkedin.com/in/muhammad-hawwin-amri-07029425a/"
                 isExternal
               >
-                LinkedIn
+                <Icon as={FaLinkedin} boxSize={3} /> LinkedIn
               </Link>
             </Box>
             <Spacer />
             <Box>
               <Link href="https://github.com/daredeviltf" isExternal>
-                Github
+                <Icon as={FaGithub} boxSize={3} /> Github
               </Link>
             </Box>
           </HStack>
         </Flex>
+        <Box>
+          <Text>Copyright 2022</Text>
+        </Box>
       </Flex>
     </Container>
   );
